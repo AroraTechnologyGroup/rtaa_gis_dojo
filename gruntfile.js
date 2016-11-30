e86bbb4db6e186fdf34219b857dcc12e244df20d
Richard Hughes <richardh522@gmail.com>
2016-11-30 11:22:13 -0500

    at work

--- gruntfile.js
+++ gruntfile.js
@@ -40,7 +40,7 @@ module.exports = function(grunt) {
     dojo: {
       dist: {
           options: {
-              releaseDir: './dist/static/home'
+              releaseDir: './dist'
           }
       },
       options: {
